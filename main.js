0308dfbf6c96027a6ef47391bc49c00cb209f294
Jake Bellacera <hi@jakebellacera.com>
2015-12-22 19:11:56 -0800

    Move window options into a variable

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  var screenWidth = Math.round(screen.width * 0.75);
-  var screenHeight = Math.round(screen.height * 0.75);
-
-  chrome.app.window.create('app.html', {
+  var windowOptions = {
     outerBounds: {
-      width: screenWidth,
-      height: screenHeight
+      width: Math.round(screen.width * 0.75),
+      height: Math.round(screen.height * 0.75)
     }
-  });
+  };
+
+  chrome.app.window.create('app.html', windowOptions);
 });
